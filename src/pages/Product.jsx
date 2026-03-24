1f2841c68a0edd7beb25b9c419910b17e3d7a0e3
Kanyin36 <kanyinsolafunmilayo36.com>
2026-03-24 12:52:52 +0100

    added a ptag to product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -4,6 +4,7 @@ import Header from "../component/Header";
 const Product = () => {
   return (
     <section className="min-h-screen w-full flex flex-col items-center justify-center">
+      <p>Added for test</p>
       <Header />
       <div className="mt-10">
         <Consume />
